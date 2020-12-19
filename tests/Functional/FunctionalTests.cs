6ab47362e2a6d8d6e9c7c79f2864ed3150e9fe49
victor.govorov <victorgovorov@mail.ru>
2020-12-19 21:31:27 +0500

    added FunctionalTests

---
+++ tests/Functional/FunctionalTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Xunit;
+
+namespace Byndyusoft.Data.Relational.Specifications.Tests.Functional
+{
+    public class DiagnosticListenerFunctionalTests : IAsyncLifetime
+    {
+        private readonly string _connectionString = "Data Source=queries.db";
+
+        public async Task InitializeAsync()
+        {
+            File.Delete("queries.db");
+
+            await using var connection = new SQLiteConnection(_connectionString);
+
+            await connection.ExecuteAsync("CREATE TABLE test (id INT, name TEXT)");
+            await connection.ExecuteAsync("INSERT INTO test (id, name) VALUES (1, 'name1')");
+            await connection.ExecuteAsync("INSERT INTO test (id, name) VALUES (2, 'name2')");
+            await connection.ExecuteAsync("INSERT INTO test (id, name) VALUES (3, 'name3')");
+
+            await connection.CloseAsync();
+        }
+
+        public Task DisposeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        [Fact]
+        public async Task Test()
+        {
+            // arrange
+            var spec = Specification.Ops.Eq("id", 1) & Specification.Create("name like @name", new {name = "%name%"});
+
+            // act
+            await using var connection = new SQLiteConnection(_connectionString);
+            var result = (await connection.QueryAsync($"SELECT id, name FROM test WHERE {spec.Sql}", spec.Params))
+                .ToArray();
+
+            // assert
+            var item = Assert.Single(result);
+            Assert.Equal(1, item!.id);
+            Assert.Equal("name1", item!.name);
+        }
+    }
+}
